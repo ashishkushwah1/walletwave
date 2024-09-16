af96b7af49b06166aabaf8c4acc8e34b30ba3004
ashishkushwah1 <ashishkushwah179@gmail.com>
2024-09-16 14:49:32 +0530

    Added vercel.json

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ const app = express();
 const router = require('./routes/index');
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: [""],
+        methods:["POST","GET","PUT"],
+        credentials:true
+    }
+));
 app.use(express.json());
 app.use('/api/v1',router);
 app.listen(3000,()=>{
