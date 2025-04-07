1490671ad04a006fe79b3a6ed044abe406be7a80
ashishkushwah1 <ashishkushwah179@gmail.com>
2025-04-08 00:33:26 +0530

    auto

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -32,16 +32,15 @@ const Profile = () => {
         setLastName('');
         setLoading(false);
     }
-    const {id} = useParams();
     useEffect(() => {
-        axios.get(`https://walletwave-peach.vercel.app/api/v1/user/bulk/${id}`).then((res) => {
+        axios.get(`https://walletwave-peach.vercel.app/api/v1/user/me`).then((res) => {
             const { user } = res.data;
             setFirstName(user.firstName);
             setLastName(user.lastName);
         }).catch((err) => {
             console.error("Error fetching user data:", err);
         });
-    },[id]);
+    },[]);
 
     return (
         <div className='flex justify-center h-screen bg-gray-100'>
